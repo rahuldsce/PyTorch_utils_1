98f8c359669a67d3dc1f96ac090f0be0caab1074
Rahul Kumar Jain <toniqapps@gmail.com>
2020-09-12 22:35:07 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,16 +2,17 @@
 # dataset.py
 
 from torchvision import datasets
+import transforms
 
 def mnist():
-  train_transforms, test_transforms = mnist_transforms()
+  train_transforms, test_transforms = transforms.mnist_transforms()
   train = datasets.MNIST('./data', train=True, download=True, transform=train_transforms)
   test = datasets.MNIST('./data', train=False, download=True, transform=test_transforms)
   return train, test
 
 
 def cifar10():
-  train_transforms, test_transforms = cifar10_transforms()
+  train_transforms, test_transforms = transforms.cifar10_transforms()
   train = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transforms)
   test = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transforms)
   return train, test
