09d0085115014a2d5110a3499a68fc2379729148
Rahul.Jain <toniqapps@gmail.com>
2020-09-19 20:11:16 +0530

    cifar 10 transform

--- transforms.py
+++ transforms.py
@@ -23,6 +23,16 @@ def mnist_transforms():
 
 # Train Phase transformations
 def cifar10_transforms():
-  transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+  transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+  ])
+
+  transform_test = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+  ])
   
-  return transform, transform
+  return transform_train, transform_test
