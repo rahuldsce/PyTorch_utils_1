bc839241a24548b3575d93bcff857fc6753bd7ee
Rahul Jain <rahul.jain@mobiledgex.com>
2024-03-29 22:25:36 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -45,7 +45,6 @@ def cifar10_albumentations():
     AUGMENTATIONS_TRAIN = A.Compose([
                                 A.RandomCrop(32, 32),
                                 A.HorizontalFlip(),
-                                A.Cutout (num_holes=1, max_h_size=8, max_w_size=8, fill_value=0.4733, always_apply=False, p=0.5),
                                 A.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
                                 ToTensorV2()
                                 ])
