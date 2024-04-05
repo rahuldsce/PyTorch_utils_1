a519ece7d2c8d19290d7d0ecc8524aa03e8c9fbd
Rahul Jain <rahul.jain@mobiledgex.com>
2024-04-06 00:00:49 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -76,3 +76,39 @@ def cifar10_albumentations():
     test = CIFAR10Sequence(x_val, y_val, transform=AUGMENTATIONS_TEST)
 
     return train, test
+
+def cifar10_s10_albumentations():
+    AUGMENTATIONS_TRAIN = A.Compose([
+                                A.CropAndPad(px=4, keep_size=False),
+                                A.HorizontalFlip(),
+                                A.CoarseDropout(max_holes=1, max_height=16, max_width=16,  # Corrected parameters
+                                                min_holes=1, min_height=16, min_width=16,   # Corrected parameters
+                                                fill_value=(0.5, 0.5, 0.5), mask_fill_value=None),
+                                A.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+                                ToTensorV2()
+                                ])
+
+    AUGMENTATIONS_TEST = A.Compose([
+                                A.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
+                                ToTensorV2()
+                                ])
+
+    data_url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    img_size = 32
+    num_channels = 3
+    num_classes = 10
+
+    cifar10DataMgr = datasetmgr.DatasetManager(data_url, "cifar-10-data", "cifar-10-batches-py", img_size, num_channels, num_classes)
+
+    # Download and extract CIFAR-10 data
+    cifar10DataMgr.maybe_download_and_extract()
+
+    # training data
+    x_train, y_train = cifar10DataMgr.load_training_data()
+    train = CIFAR10Sequence(x_train, y_train, transform=AUGMENTATIONS_TRAIN)
+
+    # Validation data
+    x_val, y_val = cifar10DataMgr.load_validation_data(5000)
+    test = CIFAR10Sequence(x_val, y_val, transform=AUGMENTATIONS_TEST)
+
+    return train, test
